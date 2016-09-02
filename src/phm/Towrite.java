625f4878dd5183a003e8f8d1a99b02ac3458200d
dpdrmj <mehul.jain.cse12@iitbhu.ac.in>
2016-09-02 12:55:55 +0530

    Data Mining Assignments
    Code taken from SPMF which is a JAVA Open-Source Data-Mining Library.
    For more info go to:
    http://www.philippe-fournier-viger.com/spmf/index.php?link=algorithms.php

---
+++ src/phm/Towrite.java
@@ -0,0 +1,17 @@
+package phm;
+
+class Towrite {
+	int[] prefix;
+	int prefixLength;
+	UtilityListPHM X;
+	double averagePeriodicity;
+	
+	Towrite(int[] prefix,int prefixLength,UtilityListPHM X,double averagePeriodicity)
+	{
+		this.prefix = prefix;
+		this.prefixLength = prefixLength;
+		this.X = X;
+		this.averagePeriodicity = averagePeriodicity;
+	}
+
+}
